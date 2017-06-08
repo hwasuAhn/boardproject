93ada86b445c58fe0ffd0778a806f2da99632bc2
Administrator <Administrator@WIN-MC9CO0SJKNL>
2017-06-08 14:06:44 +0900

    tdfd

--- myweb/src/mem/action/LogoutAction.java
+++ myweb/src/mem/action/LogoutAction.java
@@ -3,6 +3,7 @@ package mem.action;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import my.action.CommandAction;
 
@@ -10,6 +11,9 @@ public class LogoutAction extends HttpServlet implements CommandAction {
 
 	public String requestPro(HttpServletRequest req, HttpServletResponse resp)
 			throws Throwable {
+		HttpSession session = req.getSession();
+		session.invalidate();
+		
 		return "/mvc2member/logout.jsp";
 	}
 
